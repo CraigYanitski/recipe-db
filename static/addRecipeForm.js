c264f455ef51bc8b30279d15e80e5c9405a1ce66
yanitski <yanitski@ph1.uni-koeln.de>
2025-08-27 00:34:17 -0600

    added functionality for notification banners
    * still debugging connection to backend
    * colors not updating accordingly

--- static/addRecipeForm.js
+++ static/addRecipeForm.js
@@ -49,6 +49,37 @@ document.addEventListener("DOMContentLoaded", function() {
 
     addInstructionButton.addEventListener("click", addInstructionInput);
 
+    async function submitRecipe(recipe) {
+        try {
+            const response = await fetch(":8080", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(recipe)
+            });
+            if (!response.ok) {
+                throw new Error("recipe submission failed");
+            }
+            const result = await response.json();
+            console.log("recipe successfully created")
+            return result;
+        } catch (error) {
+            console.error("Error in recipe submission:", error);
+            throw error;
+        }
+    }
+
+    function showBanner(message) {
+        banner = document.getElementById("notification-banner");
+        bannerMessage = document.getElementById("notification-message");
+        bannerMessage.textContent = message;
+        banner.classList.add("show");
+        setTimeout(() => {
+            banner.classList.remove("show");
+        }, 5000);
+    }
+
     recipeForm.addEventListener("submit", function(e) {
         e.preventDefault();
 
@@ -56,6 +87,7 @@ document.addEventListener("DOMContentLoaded", function() {
         const recipeIngredients = document.querySelectorAll(".ingredint-input");
         const recipeInstructions = document.querySelectorAll(".instruction-input");
         const recipeCooktime = document.getElementById("recipe-cooktime");
+        const banner = document.getElementById("notification-banner");
 
         const ingredients = [];
         recipeIngredients.forEach(input => {
@@ -71,7 +103,24 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
 
-        alert(`Recipe ${recipeName.value.trim()} created!`);
+        const newRecipe = {
+            recipeName: recipeName.value.trim(),
+            ingredients: ingredients,
+            instructions: instructions,
+            cookTime: recipeCooktime.value
+        }
+
+        submitRecipe(newRecipe)
+            .then(data => {
+                banner.style.background = "linear-gradient(90deg, #2bff2b, 0%, #4eff4e, 100%)";
+                console.log(data);
+            })
+            .catch(error => {
+                banner.style.background = "linear-gradient(90deg, #ff2b2b, 0%, #ff4e4e, 100%)";
+                console.error(error);
+            });
+
+        showBanner(`Recipe ${recipeName.value.trim()} created!`);
     });
 });
 
