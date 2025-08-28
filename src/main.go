4cbefd8066b86706d6a35d59a38e7b5199dc3361
yanitski <yanitski@ph1.uni-koeln.de>
2025-08-28 09:14:16 -0600

    alter CORS for recipe creation

--- src/main.go
+++ src/main.go
@@ -101,7 +101,7 @@ func main() {
 func allowCreate(next http.Handler) http.Handler {
     return http.HandlerFunc(
         func(w http.ResponseWriter, r *http.Request) {
-            w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080/create")
+            w.Header().Set("Access-Control-Allow-Origin", "*")
             w.Header().Set("Access-Control-Allow-Methods", "POST")
             w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
             next.ServeHTTP(w, r)
