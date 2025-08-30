17cd881f4898df3228648ad4ecd5f3fd6c7db17f
yanitski <yanitski@ph1.uni-koeln.de>
2025-08-29 21:52:42 -0600

    fix header links

--- static/deleteRecipeForm.js
+++ static/deleteRecipeForm.js
@@ -18,27 +18,50 @@ async function sendPOST(recipe) {
     }
 }
 
-function setNotification(message, color) {
+function setNotification(message, color, href) {
     sessionStorage.setItem("showNotification", "true");
     sessionStorage.setItem("notificationMessage", message);
     sessionStorage.setItem("notificationColor", color);
-    window.location.replace("/");
+    window.location.replace(href);
+}
+
+function deleteRecipe() {
+    const recipeID = new URL(window.location.href).pathname.split("/").filter(Boolean).pop();
+    sendPOST(recipeID)
+        .then(data => {
+            setNotification(`Recipe ${recipeID} deleted!`, "green", "/");
+            console.log(data);
+        })
+        .catch(error => {
+            setNotification(`Failed to delete recipe...`, "red", "/");
+            console.error(error.message);
+        });
+}
+
+function editRecipe() {
+    window.location.reload();
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    const deleteForm = document.getElementById("delete-form");
-    deleteForm.addEventListener("submit", function(e) {
+    const linkForm = document.getElementById("recipe-link-form");
+    linkForm.addEventListener("delete", function(e) {
         e.preventDefault();
+        alert("deleting recipe");
         const recipeID = new URL(window.location.href).pathname.split("/").filter(Boolean).pop();
         sendPOST(recipeID)
             .then(data => {
-                setNotification(`Recipe ${recipeID} deleted!`, "green");
+                setNotification(`Recipe ${recipeID} deleted!`, "green", "/");
                 console.log(data);
             })
             .catch(error => {
-                setNotification(`Failed to delete recipe...`, "red");
+                setNotification(`Failed to delete recipe...`, "red", "/");
                 console.error(error.message);
             });
+    });
+    linkForm.addEventListener("edit", function(e) {
+        e.preventDefault();
+        alert("editing recipe");
+        window.location.reload();
     });
 });
 
